c88ab188f21c410f841bc26bab8ee4d5678b1928
Bohdan Holovchenko <79518893+BMHolovchenko@users.noreply.github.com>
2021-04-05 23:16:56 +0300

    Update NameParser.java

--- NameParser.java
+++ NameParser.java
@@ -1,17 +1,13 @@
-package dz6;
-
 class NameParser {
     public String parse(String[] names) {
         StringBuilder result = new StringBuilder();
         for(int i = 0; i < names.length; i++) {
-            String firstName = names[i].split(" ")[0];
-
+            String firstName = names[i].split(" ")[0];     
             result.append(firstName);
             if (i != names.length - 1) {
                 result.append(", ");
             }
         }
-
         return result.toString();
     }
 }
